6f3cdebfcd739137ea322e038289ca5926ce4f49
ImIshan7 <ishandanu999@gmail.com>
2024-01-17 14:57:11 +0530

    Content Updated -Famous Location

--- view/Home.tsx
+++ view/Home.tsx
@@ -18,6 +18,12 @@ import hikkaduwa from '../images/beach/hikkaduwa.jpg';
 import pasikudah from '../images/beach/Pasikudah.jpg';
 import tangalle from '../images/beach/tangalle.jpg';
 import negambo from '../images/beach/negambo.jpg';
+import ambuluwawa from '../images/location/ambuluwawala.jpg';
+import diyaluma from '../images/location/diyaluma.jpg';
+import meemure from '../images/location/meemure.jpg';
+import ella from '../images/location/ella.jpg';
+import forest from '../images/location/waterfall1.jpg';
+import hotain from '../images/location/hotain.png';
 export class Home extends Component {
     render() {
         return (
@@ -119,7 +125,7 @@ export class Home extends Component {
                             <li className="relative bg-white flex flex-col justify-between border rounded shadow-md hover:shadow-primary-400">
 
                                 <a className="relative" href="/tool/writey-ai">
-                                    <div className="relative w-full aspect-video">
+                                    <div className="relative w-full aspect-video ">
                                         <img className="rounded w-full h-full object-cover" src={about}/>
 
                                             <div
@@ -559,7 +565,7 @@ export class Home extends Component {
                             <li className="relative bg-white flex flex-col justify-between border rounded shadow-md hover:shadow-teal-400">
 
                                 <a className="relative" href="/tool/typewise">
-                                    <img className="rounded relative w-full object-cover aspect-video" src={negambo}/>
+                                    <img className="rounded relative w-full object-cover aspect-video " src={negambo}/>
 
 
                                 </a>
@@ -584,6 +590,96 @@ export class Home extends Component {
 
                         </ul>
 
+                    </div>
+
+                    <div className="bg-white dark:bg-gray-800 max-w-screen">
+                        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+                            <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
+                                <div className="flex items-center gap-12">
+                                    <h2 className="text-2xl font-bold text-gray-800 lg:text-4xl dark:text-white">Famous Location</h2>
+
+                                </div>
+
+                            </div>
+
+                            <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
+                                <a href="#"
+                                   className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                                    <img className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" src={ambuluwawa}/>
+
+
+                                    <div
+                                        className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                                    </div>
+
+                                    <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Ambuluwawa Tower </span>
+                                </a>
+
+
+
+                                <a href="#"
+                                   className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
+                                    <img className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" src={diyaluma}/>
+
+                                    <div
+                                        className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                                    </div>
+
+                                    <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Diyaluma Falls</span>
+                                </a>
+
+
+
+                                <a href="#"
+                                   className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
+                                    <img className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" src={meemure}/>
+
+                                    <div
+                                        className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                                    </div>
+
+                                    <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Meemure Village</span>
+                                </a>
+
+
+
+                                <a href="#"
+                                   className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                                    <img className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" src={ella}/>
+
+
+                                    <div
+                                        className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                                    </div>
+
+                                    <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Ella Rock</span>
+                                </a>
+
+                                <a href="#"
+                                   className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                                    <img className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" src={forest}/>
+
+
+                                    <div
+                                        className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                                    </div>
+
+                                    <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Sinharaja Forest Reserve</span>
+                                </a>
+                                <a href="#"
+                                   className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
+                                    <img className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" src={hotain}/>
+
+                                    <div
+                                        className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                                    </div>
+
+                                    <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Horton Plains</span>
+                                </a>
+
+
+                            </div>
+                        </div>
                     </div>
 
 
