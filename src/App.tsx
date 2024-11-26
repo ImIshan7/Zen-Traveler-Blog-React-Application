e192b6f09ef695007bfb0ee0013ff5b27b2261da
ImIshan7 <ishandanu999@gmail.com>
2024-11-26 15:05:21 +0530

    Add Files

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
-import {Home} from "./view/Home";
+
 
 function App() {
   return (
       <>
-    <BrowserRouter>
-         <Home/>
-    </BrowserRouter>
 
       </>
 
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        'ping-slow': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
+      },
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+};
